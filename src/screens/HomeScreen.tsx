539aa27eb69dd2408dc094c4a6fd76b57eec642c
lsy969999 <lsy969999@gmail.com>
2023-12-10 23:20:55 +0900

    wv message test

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, View} from 'react-native';
 import WebView from 'react-native-webview';
 import Config from 'react-native-config';
 import {getUserAgent, getVersion} from 'react-native-device-info';
@@ -32,6 +32,23 @@ const MyWebView = () => {
     };
     setWvUserAgent();
   }, []);
+
+  const onMessageHandle = async (event: any) => {
+    const {data} = event.nativeEvent;
+    const {type, msg} = JSON.parse(data);
+    console.log(`onMessageHandle_ data: ${data}`);
+    if (type === 'postMessage') {
+      postMessage({
+        test: 'test',
+        ...msg,
+      });
+    }
+  };
+
+  const postMessage = (data: any) => {
+    wvRef.current.postMessage(JSON.stringify(data));
+  };
+
   return (
     <WebView
       style={{flex: 1}}
@@ -39,6 +56,7 @@ const MyWebView = () => {
       userAgent={ua}
       webviewDebuggingEnabled={true}
       source={{uri: 'http://192.168.25.35:8080'}}
+      onMessage={onMessageHandle}
     />
   );
 };
